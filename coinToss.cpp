0677b10de0cce42517cfae626a824dfbe9ebfb26
Aditya Menon <adityamenon.2k12@gmail.com>
2017-01-18 18:01:32 +0530

    Remove unnecessary types for certain variables

--- coinToss.cpp
+++ coinToss.cpp
@@ -5,22 +5,26 @@
 
 int main ()
 {
-    uint64_t heads = 0, tails = 0, iter = 0, i ;
-    int rNum = 0, counter = 0;
-    std::cout<<" Enter number of coin tosses to be simulated ";
-    std::cin>>iter;
+    uint64_t iter = 0, i ;
+    unsigned long int heads = 0, tails = 0;
+    int rNum = 0;
+    do
+    {
+        std::cout<<" Enter number of coin tosses to be simulated ";
+        std::cin>>iter;
+    }while(iter>0);
     std::cout<<"\t\t- Progress bar - \n";
     std::cout<<"[";
     srand(time(NULL));
     for ( i = 0; ((i/(double)iter)*100)<100.0; i++ )
     {
         rNum = rand() % 2;
-        if (rNum%2==0)
+        if (rNum)
            heads++;
 
         else
             tails++;
-        if(((int)((i/(double)iter)*100))%10==0) // Print only 10 times (doesn't work)
+        if(((int)((i/(double)iter)*100))%10==0)
         {
             std::cout<<"#";
         }
