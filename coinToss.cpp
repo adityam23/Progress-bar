38411c045029cb66face31b6a98e1b461ec41ea2
Aditya Menon <adityamenon.2k12@gmail.com>
2017-01-18 14:11:58 +0530

    Update coinToss.cpp
    Changed initial values to int (from float)

--- coinToss.cpp
+++ coinToss.cpp
@@ -5,7 +5,7 @@
 
 int main ()
 {
-    uint64_t heads = 0, tails = 0, iter = 0.0, i ;
+    uint64_t heads = 0, tails = 0, iter = 0, i ;
     int rNum = 0, counter = 0;
     std::cout<<" Enter number of coin tosses to be simulated ";
     std::cin>>iter;
@@ -14,12 +14,12 @@ int main ()
     srand(time(NULL));
     for ( i = 0; ((i/(double)iter)*100)<100.0; i++ )
     {
-        rNum = rand() % 10 + 1; // rand() % 1 didn't work for some reason
+        rNum = rand() % 2;
         if (rNum%2==0)
-           heads+= 1.0;
+           heads++;
 
         else
-            tails+= 1.0;
+            tails++;
         if(((int)((i/(double)iter)*100))%10==0) // Print only 10 times (doesn't work)
         {
             std::cout<<"#";
