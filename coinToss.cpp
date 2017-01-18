3553e69532ec3d5eb994880cb34eb60087c3efbd
Aditya Menon <adityamenon.2k12@gmail.com>
2017-01-19 00:52:36 +0530

    even better progress bar (still needs fixing

--- coinToss.cpp
+++ coinToss.cpp
@@ -7,7 +7,7 @@ int main ()
 {
     uint64_t iter = 0, i ;
     unsigned long int heads = 0, tails = 0;
-    int rNum = 0, percen = 5, length = 50, len;
+    int rNum = 0, percen = 10, length = 50, len, left = 10;
     do
     {
         std::cout<<" Enter number of coin tosses to be simulated ";
@@ -24,15 +24,18 @@ int main ()
 
         else
             tails++;
-       if(percen==(int)((i/(double)iter)*100))
+       if(percen<=(int)((i/(double)iter)*100))
        {
             for(len=0;len<((percen*length)/100);len++)
                 std::cout<<"#";
-            for(;len<length;len++)
+            for(len;len<length;len++)
                 std::cout<<" ";
             std::cout<<"]"<<percen<<"% Completed"<<"\r"<<std::flush;
-            percen+=5;
+            percen+=10;
+            if(left)
+                left--;
        }
+
     }
     std::cout<<std::endl;
     std::cout<<std::endl<<" Number of heads = "<<heads;
