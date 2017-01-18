72f3587f0574c343c0cf01a94e9a926bfa3eec35
mandhyan <yash@mandhyan.in>
2017-01-18 19:12:54 +0530

    With Status bar!

--- cointoss.py
+++ cointoss.py
@@ -2,15 +2,25 @@ import random
 num=input("Enter the number of times you want to run it:")
 heads=0
 tails=0
+prog=5.0
+status=0.0
 num=int(num)
 for i in range(0,num):
 	toss=random.randint(0,1)
 	if toss == 0:
-		print "It is a heads!"
+		
 		heads=heads+1
 	else:
-		print "It is a tails!"
+		
 		tails=tails+1
+	if i==0:
+		print "[",
+	if i == int(status):
+		print "=",
+		status= prog/100.0 * num
+		prog=prog +5
+	if i==num-1:
+		print "]"
 print "Number of heads:%d"% heads
 print "Number of tails:%d"% tails
 per=0.0
